121264368e726f21ee8bdb47b4e10a4388b7016e
BINARYUPDATES.COM <binaryupdates@gmail.com>
2022-02-02 19:40:51 +0530

    Update ESP8266_Webserver_Access_Point_Mode.ino

--- ESP8266_Webserver_Access_Point_Mode.ino
+++ ESP8266_Webserver_Access_Point_Mode.ino
@@ -3,7 +3,7 @@
 
 /* Put your SSID & Password */
 const char* ssid = "NodeMCU";    // Enter SSID here
-const char* password = "12345";  // Enter Password here
+const char* password = "123456789";  // Enter Password here
 
 /* Put IP Address details */
 IPAddress local_ip(192,168,1,1);
@@ -12,23 +12,19 @@ IPAddress subnet(255,255,255,0);
 
 ESP8266WebServer server(80);
 
-bool D1Pinstatus = LOW;
-bool D2Pinstatus = LOW;
+bool LEDstatus = LOW;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(D1, OUTPUT);  // green led
-  pinMode(D2, OUTPUT);  // red led
+  pinMode(D4, OUTPUT);  
 
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
   
   server.on("/", handle_OnConnect);
-  server.on("/d1on", handle_d1on);
-  server.on("/d1off", handle_d1off);
-  server.on("/d2on", handle_d2on);
-  server.on("/d2off", handle_d2off);
+  server.on("/ledon", handle_ledon);
+  server.on("/ledoff", handle_ledoff);
   server.onNotFound(handle_NotFound);
   
   server.begin();
@@ -38,53 +34,35 @@ void setup() {
 void loop() {
   server.handleClient();
   
-  if(D1Pinstatus)
-  {digitalWrite(D1, HIGH);}
+  if(LEDstatus)
+  {digitalWrite(D4, HIGH);}
   else
-  {digitalWrite(D1, LOW);}
-  
-  if(D2Pinstatus)
-  {digitalWrite(D2, HIGH);}
-  else
-  {digitalWrite(D2, LOW);}
+  {digitalWrite(D4, LOW);}
 }
 
 void handle_OnConnect() {
-  D1Pinstatus = LOW;
-  D2Pinstatus = LOW;
-  Serial.println("LED D1: OFF | LED D2: OFF");
-  server.send(200, "text/html", updateWebpage(D1Pinstatus,D2Pinstatus)); 
+  LEDstatus = LOW;
+  Serial.println("LED: OFF");
+  server.send(200, "text/html", updateWebpage(LEDstatus)); 
 }
 
-void handle_d1on() {
-  D1Pinstatus = HIGH;
-  Serial.println("LED D1: ON");
-  server.send(200, "text/html", updateWebpage(true,D2Pinstatus)); 
+void handle_ledon() {
+  LEDstatus = HIGH;
+  Serial.println("LED: ON");
+  server.send(200, "text/html", updateWebpage(LEDstatus)); 
 }
 
-void handle_d1off() {
-  D1Pinstatus = LOW;
-  Serial.println("LED D1: OFF");
-  server.send(200, "text/html", updateWebpage(false,D2Pinstatus)); 
-}
-
-void handle_d2on() {
-  D2Pinstatus = HIGH;
-  Serial.println("LED D2: ON");
-  server.send(200, "text/html", updateWebpage(D1Pinstatus,true)); 
-}
-
-void handle_d2off() {
-  D2Pinstatus = LOW;
-  Serial.println("LED D2: OFF");
-  server.send(200, "text/html", updateWebpage(D1Pinstatus,false)); 
+void handle_ledoff() {
+  LEDstatus = LOW;
+  Serial.println("LED: OFF");
+  server.send(200, "text/html", updateWebpage(LEDstatus)); 
 }
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-String updateWebpage(uint8_t d1Pinstate,uint8_t d2Pinstate){
+String updateWebpage(uint8_t LEDstatus){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr +="<title>LED Control</title>\n";
@@ -102,16 +80,10 @@ String updateWebpage(uint8_t d1Pinstate,uint8_t d2Pinstate){
   ptr +="<h1>ESP8266 Web Server</h1>\n";
   ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
   
-   if(d1Pinstate){
-    ptr +="<p>GREEN LED: ON</p><a class=\"button button-off\" href=\"/d1off\">OFF</a>\n";
+   if(LEDstatus){
+    ptr +="<p>BLUE LED: ON</p><a class=\"button button-off\" href=\"/ledoff\">OFF</a>\n";
    }else{
-    ptr +="<p>GREEN LED: OFF</p><a class=\"button button-on\" href=\"/d1on\">ON</a>\n";
-  }
-  
-  if(d2Pinstate){
-    ptr +="<p>RED LED: ON</p><a class=\"button button-off\" href=\"/d2off\">OFF</a>\n";
-  }else{
-    ptr +="<p>RED LED: OFF</p><a class=\"button button-on\" href=\"/d2on\">ON</a>\n";
+    ptr +="<p>BLUE LED: OFF</p><a class=\"button button-on\" href=\"/ledon\">ON</a>\n";
   }
 
   ptr +="</body>\n";
