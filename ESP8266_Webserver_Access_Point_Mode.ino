bfa289773f1517feae806219159d5805e8b7d76c
BINARYUPDATES.COM <binaryupdates@gmail.com>
2022-02-22 22:07:59 +0530

    Update ESP8266_Webserver_Access_Point_Mode.ino

--- ESP8266_Webserver_Access_Point_Mode.ino
+++ ESP8266_Webserver_Access_Point_Mode.ino
@@ -2,13 +2,8 @@
 #include <ESP8266WebServer.h>
 
 /* Put your SSID & Password */
-const char* ssid = "NodeMCU";    // Enter SSID here
-const char* password = "123456789";  // Enter Password here
-
-/* Put IP Address details */
-IPAddress local_ip(192,168,1,1);
-IPAddress gateway(192,168,1,1);
-IPAddress subnet(255,255,255,0);
+const char* ssid = "NodeMCU";       
+const char* password = "123456789"; 
 
 ESP8266WebServer server(80);
 
@@ -16,11 +11,12 @@ bool LEDstatus = LOW;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(D4, OUTPUT);  
-
+  pinMode(D4, OUTPUT); 
   WiFi.softAP(ssid, password);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  delay(100);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("Access Point IP:");
+  Serial.println(myIP);
   
   server.on("/", handle_OnConnect);
   server.on("/ledon", handle_ledon);
@@ -28,7 +24,7 @@ void setup() {
   server.onNotFound(handle_NotFound);
   
   server.begin();
-  Serial.println("HTTP Server Started");
+  //Serial.println("HTTP Server Started");
 }
 
 void loop() {
